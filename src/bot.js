5f71940d6c1b26baf22caf0a6f2c962b3043eff9
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2020-09-07 17:38:44 -0300

    Meu pinto de madeira

--- src/bot.js
+++ src/bot.js
@@ -7,8 +7,37 @@ client.on("ready", () => {
     client.user.setActivity("Fazendo o Curso");
 });
 client.on("message", async (message) => {
-    if (message.content == "a") {
-        
+    var start = false;
+    var finished = false;
+    var citacao = "";
+    const autores = [];
+    for (let i = 0; i < message.content.length; i++) {
+        const e = message.content[i];
+        if (e == '"' && !start) {
+            start = true;
+            continue;
+        }
+        if (start) {
+            if (e != '"') {
+                citacao += e
+            }
+            else {
+                start = false;
+                finished = true;
+                continue;
+            }
+        }
+        if (citacao != " " && finished) {
+            var subArray = "";
+            for (let j = i; j < message.content.length; j++) {
+                subArray += message.content[j];
+            }
+            const u = subArray.split(",");
+            autores.push(...u);
+            break;
+        }
     }
+    console.log(citacao);
+    console.log(autores);
 });
 client.login(process.env.DISCORD_TOKEN);
