7b66512264a046f332056e4eed1dc32e3cf90c87
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2020-09-07 14:05:06 -0300

    Configurado bot padrão

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,16 @@
 require("dotenv/config");
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
+client.on("ready", () => {
+    console.log("Bot começou CARALHO");
+    client.user.setActivity("Fazendo o Curso");
+});
+
+client.on("message", (message) => {
+
+    message.channel.name === "livro-de-citações" ? console.log("foi") : console.log("não foi")
+});
+
+client.login(process.env.DISCORD_TOKEN);
 console.log(process.env.DISCORD_TOKEN);
